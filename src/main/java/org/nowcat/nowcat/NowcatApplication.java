c8871c1fc1c93aa664b9e921ab90e3fb864226c1
ggm77 <shmshm86@naver.com>
2025-12-26 00:53:28 +0900

    first commit

---
+++ src/main/java/org/nowcat/nowcat/NowcatApplication.java
@@ -0,0 +1,13 @@
+package org.nowcat.nowcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowcatApplication {
+	// 2025.12.26 개발 시작
+	public static void main(String[] args) {
+		SpringApplication.run(NowcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/nowcat/nowcat/NowcatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nowcat.nowcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
